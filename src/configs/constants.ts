5fdfcffc18d6a45007d24711ff0f9f368b6c550c
Karthikayan <skarthikayan7@gmail.com>
2024-08-24 16:08:54 +0530

    Feature: swagger api docs

--- src/configs/constants.ts
+++ src/configs/constants.ts
@@ -1,3 +1,7 @@
 export default {
   baseUrl: '/api/v1',
+  title: 'AgilePlus',
+  description:
+    "A web application for intuitive and simple project management. It helps to plan, track and manages a team's workflow effectively.",
+  email: 'skarthikayan7@gmail.com',
 };
